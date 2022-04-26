e3b912002dc2321823c8cac4b3fa09f229dd13f1
ahanafislam <ahanafislam@gmail.com>
2022-04-27 04:52:00 +0600

    Create post api for order collection

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geneusCar').collection('service');
+        const orderCollection = client.db('geneusCar').collection('order');
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -45,6 +46,12 @@ async function run() {
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
         })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
     }
     finally {
         // await client.close();
